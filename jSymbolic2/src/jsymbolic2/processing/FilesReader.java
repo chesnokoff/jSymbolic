3c2a85f2a70f09392a054ee8bf6f466185ea3656
chesnokoff <chesnokoff239@gmail.com>
2024-03-06 19:44:51 +0300

    FilesReader class added. It gives Sequences from files

---
+++ jSymbolic2/src/jsymbolic2/processing/FilesReader.java
@@ -0,0 +1,110 @@
+package jsymbolic2.processing;
+
+import ca.mcgill.music.ddmal.mei.MeiXmlReader;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.ddmal.jmei2midi.MeiSequence;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesReader {
+    public ArrayList<MutablePair<String, Sequence>> extractMidi(List<File> files) throws Exception {
+        ArrayList<MutablePair<String, Sequence>> sequences = new ArrayList<>(files.size());
+        for (File file : files) {
+            sequences.add(MutablePair.of(file.getName(), extractMidi(file)));
+        }
+        return sequences;
+    }
+
+    public ArrayList<MutablePair<String, MeiSequence>> extractMei(List<File> files) throws Exception {
+        ArrayList<MutablePair<String, MeiSequence>> sequences = new ArrayList<>(files.size());
+        for (File file : files) {
+            sequences.add(MutablePair.of(file.getName(), extractMei(file)));
+        }
+        return sequences;
+    }
+
+    public Sequence extractMidi(File file) throws Exception {
+        if (!file.exists() || file.isDirectory() || !file.canRead()) {
+            throw new IllegalArgumentException("File " + file.getPath() + " is invalid.");
+        }
+        Sequence sequence;
+        if (isValidMidiFile(file)) {
+            sequence = getMidiSequenceFromMidiFile(file);
+        } else if (isValidMeiFile(file)) {
+            sequence = getMidiSequenceFromMeiFile(file);
+        } else {
+            throw new Exception("The specified file, " + file + ", is not a valid MIDI or MEI file.");
+        }
+        return sequence;
+    }
+
+    public MeiSequence extractMei(File file) throws Exception {
+        if (!file.exists() || !file.isDirectory() || !file.canRead()) {
+            throw new IllegalArgumentException("File " + file.getPath() + " is invalid.");
+        }
+        MeiSequence sequence;
+        if (isValidMeiFile(file))
+            sequence = getMeiSequenceFromMeiFile(file);
+        else {
+            throw new Exception("The specified file, " + file + ", is not a valid MEI file.");
+        }
+        return sequence;
+    }
+
+
+    private Sequence getMidiSequenceFromMidiFile(File file)
+            throws IOException, InvalidMidiDataException {
+        Sequence sequence;
+        try {
+            sequence = MidiSystem.getSequence(file);
+        } catch (IOException e) {
+            throw e;
+        }
+        return sequence;
+    }
+
+    private Sequence getMidiSequenceFromMeiFile(File file)
+            throws MeiXmlReader.MeiXmlReadException, InvalidMidiDataException {
+        Sequence sequence = null;
+        try {
+            MeiSequence meiSequence = new MeiSequence(file);
+            sequence = meiSequence.getSequence();
+        } catch (MeiXmlReader.MeiXmlReadException | InvalidMidiDataException e) {
+            throw e;
+        }
+        return sequence;
+    }
+
+    private MeiSequence getMeiSequenceFromMeiFile(File file)
+            throws InvalidMidiDataException, MeiXmlReader.MeiXmlReadException {
+        try {
+            return new MeiSequence(file);
+        } catch (InvalidMidiDataException | MeiXmlReader.MeiXmlReadException e) {
+            throw e;
+        }
+    }
+
+    private boolean isValidMidiFile(File file) {
+        try {
+            MidiSystem.getMidiFileFormat(file);
+        } catch (InvalidMidiDataException | IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidMeiFile(File file) {
+        try {
+            MeiXmlReader.loadFile(file);
+        } catch (MeiXmlReader.MeiXmlReadException e) {
+            return false;
+        }
+        return true;
+    }
+}

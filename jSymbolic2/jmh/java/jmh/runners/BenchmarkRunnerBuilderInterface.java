612bfbe3757f2a5d6bf8776bb25a7be5af472ddf
chesnokoff <chesnokoff239@gmail.com>
2024-04-02 14:21:16 +0300

    Some documentation added for better understanding

--- jSymbolic2/jmh/java/jmh/runners/BenchmarkRunnerBuilderInterface.java
+++ jSymbolic2/jmh/java/jmh/runners/BenchmarkRunnerBuilderInterface.java
@@ -1,5 +1,11 @@
 package jmh.runners;
 
+/**
+ * This interface implements building Benchmark runner
+ */
 public interface BenchmarkRunnerBuilderInterface {
+    /**
+     * @return Returns benchmark runner that was built
+     */
     BenchmarkRunnerInterface createBenchmarkRunner();
 }

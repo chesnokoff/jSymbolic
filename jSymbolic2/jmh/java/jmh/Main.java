e0076390202213516040f895414d8842713bfd61
chesnokoff <chesnokoff239@gmail.com>
2024-04-05 12:02:52 +0300

    CLI support added

--- jSymbolic2/jmh/java/jmh/Main.java
+++ jSymbolic2/jmh/java/jmh/Main.java
@@ -1,17 +1,19 @@
 package jmh;
 
 
+import jmh.process.CommandParser;
 import jmh.runners.BenchmarkRunnerBuilder;
 import jmh.runners.BenchmarkRunnerInterface;
 
 public class Main {
 
     public static void main(String[] args) {
-        BenchmarkRunnerInterface benchmarkRunnerSingleThread = new BenchmarkRunnerBuilder().createBenchmarkRunner();
-        BenchmarkRunnerInterface benchmarkRunnerMultithreading = new BenchmarkRunnerBuilder()
-                .setThreadsNumber(5)
-                .createBenchmarkRunner();
-        benchmarkRunnerSingleThread.run();
-        benchmarkRunnerMultithreading.run();
+        CommandParser cmd = new CommandParser(args);
+        if (cmd.askedHelp()) {
+            cmd.printHelp();
+            return;
+        }
+        BenchmarkRunnerInterface benchmarkRunner = cmd.generateBenchmarkBuilder();
+        benchmarkRunner.run();
     }
 }

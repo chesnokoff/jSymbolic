6a6ad0347d7f558a7f8a20666bf014acd98ac5c1
chesnokoff <chesnokoff239@gmail.com>
2024-10-05 18:56:12 +0300

    Refactor BeatHistogramTempoStandardizedFeature to replace manual array copy with System.arraycopy

--- jSymbolic2/src/jsymbolic2/features/BeatHistogramTempoStandardizedFeature.java
+++ jSymbolic2/src/jsymbolic2/features/BeatHistogramTempoStandardizedFeature.java
@@ -67,8 +67,7 @@ public class BeatHistogramTempoStandardizedFeature
 		if (sequence_info != null)
 		{
 			result = new double[sequence_info.beat_histogram_120_bpm_standardized.length - 40];
-			for (int i = 0; i < result.length; i++)
-				result[i] = sequence_info.beat_histogram_120_bpm_standardized[i + 40];
+            System.arraycopy(sequence_info.beat_histogram_120_bpm_standardized, 40, result, 0, result.length);
 		}
 		return result;
 	}

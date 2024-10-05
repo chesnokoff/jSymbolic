a7133c160b2ceac11c0774d1470532943241703e
chesnokoff <chesnokoff239@gmail.com>
2024-10-05 17:29:59 +0300

    Refactor MIDIIntermediateRepresentations to remove redundant box class

--- jSymbolic2/src/jsymbolic2/processing/MIDIIntermediateRepresentations.java
+++ jSymbolic2/src/jsymbolic2/processing/MIDIIntermediateRepresentations.java
@@ -956,9 +956,9 @@ public class MIDIIntermediateRepresentations
 						if (!key_sig_found)
 						{
 							if (data[1] == 0) // major
-								overall_metadata[0] = new Integer(0);
+								overall_metadata[0] = 0;
 							else if (data[1] == 1) // minor
-								overall_metadata[0] = new Integer(1);
+								overall_metadata[0] = 1;
 								key_sig_found = true;
 						}
 					}
@@ -1630,7 +1630,7 @@ public class MIDIIntermediateRepresentations
 								duration += duration_of_ticks_in_seconds[i];
 
 							// Add note to list
-							note_durations.add(new Double(duration));
+							note_durations.add(duration);
 						}
 					}
 				}

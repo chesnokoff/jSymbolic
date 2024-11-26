3300c93ac6530a46915925cc5d469bdbc100d3bc
chesnokoff <chesnokoff239@gmail.com>
2024-11-26 21:58:18 +0300

    Add Feature interface

---
+++ jSymbolic2/refactoring/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.script.RefactorExtractorsToFeature;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new RefactorExtractorsToFeature().run();
+    }
+}

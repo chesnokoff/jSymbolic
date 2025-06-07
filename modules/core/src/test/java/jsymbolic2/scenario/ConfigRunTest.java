9b2aa81410752f733d6b3aed45746bfc343e96df
chesnokoff <chesnokoff239@gmail.com>
2025-06-07 20:03:32 +0300

    Update ConfigRunTest: now it does not care about order of features

--- modules/core/src/test/java/jsymbolic2/scenario/ConfigRunTest.java
+++ modules/core/src/test/java/jsymbolic2/scenario/ConfigRunTest.java
@@ -7,15 +7,19 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConfigRunTest {
+
+    private static final double DELTA = 0.00001;
+
     @Test
     public void testAllFeaturesConfigRun() throws Exception {
-        Main.main(new String[]{"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/all_features_config.txt"});
+        Main.main(new String[] {"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/all_features_config.txt"});
         String prefix = "src/test/java/jsymbolic2/scenario/resources/values/";
         assertTrue(new File(prefix + "all_features_definitions_expected.xml").exists());
         assertTrue(new File(prefix + "all_features_values_expected.xml").exists());
@@ -23,52 +27,100 @@ public class ConfigRunTest {
         assertTrue(new File(prefix + "all_features_values_actual.xml").exists());
         DataBoard dataBoardExpected = new DataBoard(null,
             prefix + "all_features_definitions_expected.xml",
-                new String[]{prefix + "all_features_values_expected.xml"}, null);
+            new String[] {prefix + "all_features_values_expected.xml"}, null);
         DataBoard dataBoardActual = new DataBoard(null,
             prefix + "all_features_definitions_actual.xml",
-                new String[]{prefix + "all_features_values_actual.xml"}, null);
-        for (int i = 0; i < dataBoardExpected.feature_vectors.length; i++) {
-            for (int j = 0; j < dataBoardExpected.feature_vectors[i].feature_values.length; j++) {
-                assertArrayEquals(dataBoardExpected.feature_vectors[i].feature_values[j], dataBoardActual.feature_vectors[i].feature_values[j], 0.001,
-                        dataBoardExpected.feature_vectors[i].feature_names[j] + " feature is not the same.");
-            }
-        }
+            new String[] {prefix + "all_features_values_actual.xml"}, null);
+        assertTrue(equals(dataBoardExpected, dataBoardActual));
     }
 
     @Test
     public void testWindowFeaturesConfigRun() throws Exception {
-        Main.main(new String[]{"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/window_config.txt"});
+        Main.main(new String[] {"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/window_config.txt"});
         DataBoard dataBoardExpected = new DataBoard(null,
-                "src/test/java/jsymbolic2/scenario/resources/values/window_definitions_expected.xml",
-                new String[]{"src/test/java/jsymbolic2/scenario/resources/values/window_values_expected.xml"}, null);
+            "src/test/java/jsymbolic2/scenario/resources/values/window_definitions_expected.xml",
+            new String[] {"src/test/java/jsymbolic2/scenario/resources/values/window_values_expected.xml"}, null);
         DataBoard dataBoardActual = new DataBoard(null,
-                "src/test/java/jsymbolic2/scenario/resources/values/window_definitions_actual.xml",
-                new String[]{"src/test/java/jsymbolic2/scenario/resources/values/window_values_actual.xml"}, null);
-        for (int i = 0; i < dataBoardExpected.feature_vectors.length; i++) {
-            for (int j = 0; j < dataBoardExpected.feature_vectors[i].sub_sets.length; j++) {
-                for (int k = 0; k < dataBoardExpected.feature_vectors[i].sub_sets[j].feature_values.length; k++) {
-                        assertArrayEquals(dataBoardExpected.feature_vectors[i].sub_sets[j].feature_values[k],
-                            dataBoardExpected.feature_vectors[i].sub_sets[j].feature_values[k], 0.001,
-                            dataBoardExpected.feature_vectors[i].sub_sets[j].feature_names[k] + " feature is not the same.");
-                    }
-                }
-        }
+            "src/test/java/jsymbolic2/scenario/resources/values/window_definitions_actual.xml",
+            new String[] {"src/test/java/jsymbolic2/scenario/resources/values/window_values_actual.xml"}, null);
+        assertTrue(equals(dataBoardExpected, dataBoardActual));
     }
 
     @Test
     public void testOverallFeaturesConfigRun() throws Exception {
-        Main.main(new String[]{"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/overall_config.txt"});
+        Main.main(new String[] {"-configrun", "src/test/java/jsymbolic2/scenario/resources/configs/overall_config.txt"});
         DataBoard dataBoardExpected = new DataBoard(null,
-                "src/test/java/jsymbolic2/scenario/resources/values/overall_definitions_expected.xml",
-                new String[]{"src/test/java/jsymbolic2/scenario/resources/values/overall_values_expected.xml"}, null);
+            "src/test/java/jsymbolic2/scenario/resources/values/overall_definitions_expected.xml",
+            new String[] {"src/test/java/jsymbolic2/scenario/resources/values/overall_values_expected.xml"}, null);
         DataBoard dataBoardActual = new DataBoard(null,
-                "src/test/java/jsymbolic2/scenario/resources/values/overall_definitions_actual.xml",
-                new String[]{"src/test/java/jsymbolic2/scenario/resources/values/overall_values_actual.xml"}, null);
-        for (int i = 0; i < dataBoardExpected.feature_vectors.length; i++) {
-            for (int j = 0; j < dataBoardExpected.feature_vectors[i].feature_values.length; j++) {
-                assertArrayEquals(dataBoardExpected.feature_vectors[i].feature_values[j], dataBoardActual.feature_vectors[i].feature_values[j], 0.001,
-                    dataBoardExpected.feature_vectors[i].feature_names[j] + " feature is not the same.");
+            "src/test/java/jsymbolic2/scenario/resources/values/overall_definitions_actual.xml",
+            new String[] {"src/test/java/jsymbolic2/scenario/resources/values/overall_values_actual.xml"}, null);
+        assertTrue(equals(dataBoardExpected, dataBoardActual));
+    }
+
+    private static boolean equals(DataBoard board, DataBoard anotherBoard) {
+        for (DataSet audio : board.feature_vectors) {
+            Optional<DataSet> anotherAudio = Arrays.stream(anotherBoard.feature_vectors)
+                .filter(a -> a.identifier.equals(audio.identifier))
+                .findFirst();
+
+            if (anotherAudio.isEmpty()) {
+                return false;
+            }
+
+            if (!equals(audio, anotherAudio.get())) {
+                return false;
             }
         }
+        return true;
+    }
+
+    private static boolean equals(DataSet audio, DataSet anotherAudio) {
+        if (audio.sub_sets != null) {
+            if (anotherAudio.sub_sets == null) {
+                return false;
+            }
+            for (int i = 0; i < audio.sub_sets.length; i++) {
+                if (!equals(audio.sub_sets[i], anotherAudio.sub_sets[i])) {
+                    return false;
+                }
+            }
+        }
+
+        if (audio.feature_values == null) {
+            return anotherAudio.feature_values == null;
+        }
+
+        if (audio.feature_values.length != anotherAudio.feature_values.length) {
+            return false;
+        }
+
+        Map<String, double[]> features = new HashMap<>();
+        Map<String, double[]> anotherFeatures = new HashMap<>();
+
+        for (int i = 0; i < audio.feature_values.length; i++) {
+            features.put(audio.feature_names[i], audio.feature_values[i]);
+            anotherFeatures.put(anotherAudio.feature_names[i], anotherAudio.feature_values[i]);
+        }
+
+        if (features.size() != anotherFeatures.size()) {
+            return false;
+        }
+
+        for (Map.Entry<String, double[]> entry : features.entrySet()) {
+            String key = entry.getKey();
+            double[] value = entry.getValue();
+            double[] anotherValue = anotherFeatures.get(key);
+            if (anotherValue == null || value.length != anotherValue.length) {
+                return false;
+            }
+            for (int i = 0; i < value.length; i++) {
+                if (Math.abs(value[i] - anotherValue[i]) >= DELTA) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
